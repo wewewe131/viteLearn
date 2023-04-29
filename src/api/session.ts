33c13eecafd863c256b8f30c09803173f0221f21
wewewe131 <2283204413@qq.com>
2023-04-29 12:08:59 +0800

    前端第一次提交

---
+++ src/api/session.ts
@@ -0,0 +1,15 @@
+import request from "@/utils/request";
+
+export default {
+
+    getSessionList: () => request.get('/session/getSessionList'),
+
+    createSession: (data: {
+        "lastMessage"?: any,
+        "userId"?: any,
+        "receiveId"?: any,
+        "receiveName"?: any,
+        "sessionType"?: any,
+    }) => request.post('/session/createSession', data),
+    
+}
