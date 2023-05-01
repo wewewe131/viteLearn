2eb7e4e8f43b7ef1ff248802056456090b8f25a0
wewewe131 <2283204413@qq.com>
2023-05-01 14:29:33 +0800

    完成

--- src/api/login.ts
+++ src/api/login.ts
@@ -4,9 +4,8 @@ export default {
     login: (userId: string, password: string) => request.post("/userAuth", { userId, password }),
     regist: (uname: string, password: string, usex: string) => request.post("/userAuth/register", { uname, password, usex }),
     getUserInfo: () => request.get("/user"),
-    logout: () => request.delete("/userAuth"),
+    logout: () => request.post("/userAuth/logout", {}),
     updateUserInfo: (data: any) => request.put("/user", data),
     updateAvatar: (formData: any) => request.upload("/reAvatar", formData),
     rePassword: (oldPassword: string, newPassword: string) => request.put("/user/rePassword", { oldPassword, newPassword }),
-    
 }
